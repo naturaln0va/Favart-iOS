7a258c0dba555f58745affccd95c200d9e7cabeb
Ryan Ackermann <ryan@ackermann.io>
2018-12-22 21:02:27 -0600

    improved the container app

--- Favart/ViewController.swift
+++ Favart/ViewController.swift
@@ -6,20 +6,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .magenta
-        
-        let prettyLabel = UILabel()
-        prettyLabel.translatesAutoresizingMaskIntoConstraints = false
-        prettyLabel.font = UIFont(name: "Chalkduster", size: 64)
-        prettyLabel.textColor = .green
-        prettyLabel.text = "Kool App"
-        
-        view.addSubview(prettyLabel)
-        
-        NSLayoutConstraint.activate([
-            prettyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            prettyLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
     }
 
 }
