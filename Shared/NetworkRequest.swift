5418963d723383e1b6d993a7377b4e6c6ca84ce7
Ryan Ackermann <ryan@ackermann.io>
2019-01-09 18:20:35 -0600

    added RW copyright info to the project

--- Shared/NetworkRequest.swift
+++ Shared/NetworkRequest.swift
@@ -1,8 +1,34 @@
+/// Copyright (c) 2018 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import Foundation
 
 final class NetworkRequest: Operation {
-  
   enum State: String {
     case ready, executing, finished
     
@@ -63,13 +89,11 @@ final class NetworkRequest: Operation {
       print("<\(method.rawValue) \(urlString)> \(info).")
     }
   }
-  
 }
 
 // MARK: - NSOperation Overrides
 
 extension NetworkRequest {
-  
   override var isReady: Bool {
     return super.isReady && state == .ready
   }
@@ -128,13 +152,11 @@ extension NetworkRequest {
     sessionTask?.cancel()
     state = .finished
   }
-  
 }
 
 // MARK: - NSURLSession Delegate
 
 extension NetworkRequest: URLSessionDataDelegate {
-  
   func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
     if isCancelled {
       state = .finished
@@ -188,5 +210,4 @@ extension NetworkRequest: URLSessionDataDelegate {
     
     state = .finished
   }
-  
 }
